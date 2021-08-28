1c8f3657ac9bbd212b91cb7c25d2c0b94cf6a39f
Chris Chandler <c_j_c_1993@hotmail.co.uk>
2021-08-28 15:56:45 +0100

    Init project

---
+++ src/main/java/com/project/munroapi/MunroapiApplication.java
@@ -0,0 +1,13 @@
+package com.project.munroapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MunroapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MunroapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/munroapi/MunroapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.munroapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MunroapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
